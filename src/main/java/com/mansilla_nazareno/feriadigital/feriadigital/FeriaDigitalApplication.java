7b0846b576944f4bde27f5b69d895bd2d75c525f
nazareno28210 <nazarenoguardia2004@gmail.com>
2025-10-14 11:12:46 -0300

    primer commit

---
+++ src/main/java/com/mansilla_nazareno/feriadigital/feriadigital/FeriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.mansilla_nazareno.feriadigital.feriadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mansilla_nazareno/feriadigital/feriadigital/FeriaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansilla_nazareno.feriadigital.feriadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
